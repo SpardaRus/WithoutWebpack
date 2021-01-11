c2bc20f48bf93e824820d95832248664bd14e227
SpardaRus <ale-altair@yandex.ru>
2021-01-11 21:44:22 +0400

    6 add import css to index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,10 @@
 import * as $ from 'jquery'
 import * as dateUtils from './dateUtils'
 
+import '../css/container.css'
+import '../css/content.css'
+import '../css/menu.css'
+
 $(document).ready(function () {
     $("#buttonSetText").click(function () {
         let dateNow = dateUtils.getCurrentDate();
