7e2dffdb7004a0e61e576808576b236aa5321882
SpardaRus <ale-altair@yandex.ru>
2021-01-11 21:40:35 +0400

    5 add context: path.resolve to webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,22 +3,25 @@ const HTMLWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = { //nodejs
     mode: "development",//mode build (development for build full script) (production for build optimization script)
-    entry: path.resolve(__dirname, 'src/js/index.js'), // main js file
+    context: path.resolve(__dirname, 'src'),
+    entry: './js/index.js', // main js file
     output: {
         filename: 'bundle.js', //name for build output file
         path: path.resolve(__dirname, 'dist') //path for build output file
     },
     plugins: [
         new HTMLWebpackPlugin({
-            template: path.resolve(__dirname, 'src/html/index.html')
+            template: './html/index.html'
         }),
         new HTMLWebpackPlugin({
             filename: "work.html",
-            template: path.resolve(__dirname, 'src/html/work.html')
+            template: './html/work.html'
         }),
         new HTMLWebpackPlugin({
             filename: "about.html",
-            template: path.resolve(__dirname, 'src/html/about.html')
-        })]
+            template: './html/about.html'
+        })
+    ]
+
 
 }
